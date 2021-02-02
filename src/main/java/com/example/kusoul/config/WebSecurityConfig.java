5ab2584fa3448a0ef3ea645a9c7624b6cc869c97
kusoullv <kusoul411029876@outlook.com>
2021-02-02 11:07:47 +0800

    commit

--- src/main/java/com/example/kusoul/config/WebSecurityConfig.java
+++ src/main/java/com/example/kusoul/config/WebSecurityConfig.java
@@ -1,23 +1,38 @@
 package com.example.kusoul.config;
 
 import com.example.kusoul.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Resource
@@ -46,6 +61,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        String encodedPassword = passwordEncoder.encode("123");
+        System.out.println(encodedPassword);
+
         http.authorizeRequests()
                 .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
                     @Override
@@ -57,9 +78,84 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 .and()
                 .formLogin()
-                .loginProcessingUrl("/login").permitAll()
+                .loginProcessingUrl("/login")
+                .usernameParameter("name")
+                .passwordParameter("password")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(
+                            HttpServletRequest request,
+                            HttpServletResponse response,
+                            Authentication authentication) throws IOException, ServletException {
+                        Object principal = authentication.getPrincipal();
+                        response.setContentType("application/json;charset=utf-8");
+                        PrintWriter pw = response.getWriter();
+                        response.setStatus(200);
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("status",200);
+                        map.put("msg",principal);
+                        ObjectMapper om = new ObjectMapper();
+                        pw.write(om.writeValueAsString(map));
+                        pw.flush();
+                        pw.close();
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        response.setContentType("application/json;charset=utf-8");
+                        response.setStatus(401);
+                        PrintWriter pw =  response.getWriter();
+                        Map<String,Object> map = new HashMap<>();
+                        map.put("status", 401);
+                        if (exception instanceof UsernameNotFoundException) {
+                            map.put("msg", "当前用户不存在!");
+                        }
+                        else if (exception instanceof LockedException) {
+                            map.put("msg", "用户被锁定, 登录失败!");
+                        }
+                        else if  (exception instanceof BadCredentialsException) {
+                            map.put("msg", "用户名或密码输入错误，登录失败!");
+                        }
+                        else if (exception instanceof DisabledException) {
+                            map.put("msg", "用户被禁用，登录失败!");
+                        }
+                        else if (exception instanceof AccountExpiredException) {
+                            map.put("msg", "用户已过期，登录失败!");
+                        }
+                        else if (exception instanceof CredentialsExpiredException) {
+                            map.put("msg", "密码已过期，登录失败!");
+                        } else {
+                            map.put("msg", "登录失败！");
+                        }
+
+                        ObjectMapper om = new ObjectMapper();
+                        pw.write(om.writeValueAsString(map));
+                        pw.flush();
+                        pw.close();
+                    }
+                })
+                .permitAll()
                 .and()
-                .csrf().disable();
+                .logout()
+                .logoutUrl("/logout")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login_page");
+                    }
+                })
+                .and()
+                .csrf().
+                disable();
     }
     @Bean
     CustomFilterInvocationSecurityMetadataSource cfisms() {
